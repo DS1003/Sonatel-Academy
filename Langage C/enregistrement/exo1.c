7b0679cdf1f7480a52758c8e956da57e7a600cee
DS1003 <mouhaleecr7@gmail.com>
2024-03-05 00:22:23 +0000

    feat update

--- Langage C/enregistrement/exo1.c
+++ Langage C/enregistrement/exo1.c
@@ -9,6 +9,7 @@ typedef struct {
 
 Point saisie_point ();
 void afficher_point (Point P);
+float distance (Point A, Point B);
 
 int  main() {
     Point a, b;
@@ -20,9 +21,11 @@ int  main() {
     b = saisie_point(); 
     afficher_point(b);
 
+    float dist = distance(a, b);
+    printf("\n--------------\nLa distance entre le Point A et le Point B est:  %.1f \n",dist);
+
     return 0;
 }
-
 
 Point saisie_point () {
     Point P;
@@ -35,6 +38,19 @@ Point saisie_point () {
   
     return P;
 }
+
+//Dist(A,B)=√(〖(Xb-Xa)〗^2+〖(Yb-Ya)〗^2 )
+float racinecarre (float x) {return sqrt(x);}
+float distance (Point A, Point B) {
+    float Xdif=racinecarre(pow((B.abscisse_x - A.abscisse_x), 2));
+    float Ydif=racinecarre(pow((B.ordonnée_y - A.ordonnée_y), 2));
+    return (Xdif + Ydif);
+}
+
+void affiche_distance (Point A, Point B) {
+    printf("Distance entre les points %f \n", distance(A,B));
+}
+
 void afficher_point (Point P) {
     printf("L'abscisse est %.1f et l'ordonnée est %.1f.\n", P.abscisse_x, P.ordonnée_y);
 }
